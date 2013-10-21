bb5cd68b5e390c249574b5b4383f1033512a29b0
Troy Murray <troy.murray@gmail.com>
2013-10-21 10:10:18 -0700

    Update express to accept HTTP POST with a variable named message and display to the user

--- express.js
+++ express.js
@@ -1,8 +1,20 @@
 var express = require('express'),
-  app = express();
+    app = express(),
+    text = 'Hello World.';
 
+//Some Server configuration
+app.use(express.bodyParser());
+
+//Get latest movie
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.send(text);
 });
 
+//Save a movie, set it to the latest
+app.post('/', function(req, res) {
+  text = req.body.message;
+  res.send('Text set to "' + text + '"');
+});
+
+//Start the app
 app.listen(2013);
